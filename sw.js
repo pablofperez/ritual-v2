3fd944f7985035a6b04a9b503414543d78040b76
pablofperez <pablofperez@hotmail.com>
2025-04-09 19:46:52 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME = 'ritual-cache-v1';
-const urlsToCache = ['/', '/index.html', '/style.css', '/script.js', '/manifest.json'];
-self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))); });
-self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => { if (key !== CACHE_NAME) return caches.delete(key); })))); });
-self.addEventListener('fetch', event => { event.respondWith(caches.match(event.request).then(response => response || fetch(event.request).then(res => caches.open(CACHE_NAME).then(cache => { cache.put(event.request, res.clone()); return res; })))); });
